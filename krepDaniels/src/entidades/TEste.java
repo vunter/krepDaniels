09dffcd5fff841deb28855a6d138e3a4785b42d6
vunter <leoeifert@hotmail.com>
2018-12-28 20:02:48 -0200

    uh

---
+++ krepDaniels/src/entidades/TEste.java
@@ -0,0 +1,11 @@
+package entidades;
+
+import entidades.Funcionario;
+
+public class TEste {
+
+	public static void main(String[] args) {
+		Funcionario f = new Funcionario("Leo", "leoeifert", "123", 29, 12, 1999);
+	}
+
+}
--- krepDaniels/src/telas/TelaLogin.java
+++ krepDaniels/src/telas/TelaLogin.java
@@ -1,47 +1,43 @@
 package telas;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.GridLayout;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import bancoDeDados.Conexao;
 
-import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
-import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
-public class TelaLogin {
+public class TelaLogin extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
 	private JTextField textField;
 	private JPasswordField passwordField;
-	Conexao conn = new Conexao();
+	private Conexao conn = new Conexao();
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-
+	public void starta() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TelaLogin window = new TelaLogin();
-					window.frame.setVisible(true);
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -49,88 +45,84 @@ public class TelaLogin {
 		});
 	}
 
-	/**
-	 * Create the application.
+	/*
+	 * Create the frame.
 	 */
 	public TelaLogin() {
-		initialize();
-	}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 287, 241);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 269, 203);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JLabel lblFaaLogin = new JLabel("LOGIN");
-		lblFaaLogin.setFont(new Font("Tahoma", Font.PLAIN, 35));
-		
+		JLabel lblLogin = new JLabel("LOGIN");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 77));
+
 		JLabel lblUsurio = new JLabel("Usu\u00E1rio:");
-		
+		lblUsurio.setFont(new Font("Tahoma", Font.PLAIN, 18));
+
 		textField = new JTextField();
 		textField.setColumns(10);
-		
+
 		JLabel lblSenha = new JLabel("Senha:");
-		
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 18));
+
 		passwordField = new JPasswordField();
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
+
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				conn.setUsuario(textField.getText());
 				conn.setSenha(String.valueOf(passwordField.getPassword()));
 				conn.conectar(textField.getText(), String.valueOf(passwordField.getPassword()));
-				JOptionPane.showMessageDialog(null, conn.estaConectado(), "Resumo da Conexão", JOptionPane.INFORMATION_MESSAGE);
-				/*
-				 if (!conn.connection.equals(null)) {
-				 
-				conn.desconectar();
-				JOptionPane.showMessageDialog(null, "Desconectado", "Desconexão", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, conn.estaConectado(), "Resumo da Conexão",
+						JOptionPane.INFORMATION_MESSAGE);
+
+				if (!conn.connection.equals(null)) {
+					dispose();
+					TelaPrincipal tPrincipal = new TelaPrincipal();
+					tPrincipal.setVisible(true);
+
 				} else {
 					JOptionPane.showMessageDialog(null, "teste");
 				}
-			*/
-				
 			}
 		});
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblUsurio)
-							.addGap(17)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblFaaLogin, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGap(7)
-									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-										.addComponent(passwordField, Alignment.LEADING)
-										.addComponent(textField, Alignment.LEADING)
-										.addComponent(btnLogin, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)))))
-						.addComponent(lblSenha))
-					.addContainerGap(64, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(lblFaaLogin, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
-					.addGap(11)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblUsurio, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblSenha)
-						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnLogin)
-					.addContainerGap(16, Short.MAX_VALUE))
-		);
-		frame.getContentPane().setLayout(groupLayout);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane
+				.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup().addGap(18)
+								.addComponent(lblLogin, GroupLayout.DEFAULT_SIZE, 297, Short.MAX_VALUE).addGap(109))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup().addGroup(gl_contentPane
+								.createParallelGroup(Alignment.TRAILING).addGroup(Alignment.LEADING,
+										gl_contentPane.createSequentialGroup().addGap(62).addComponent(btnEntrar,
+												GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE))
+								.addGroup(gl_contentPane.createSequentialGroup().addContainerGap()
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+												.addComponent(lblUsurio, GroupLayout.DEFAULT_SIZE,
+														GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(lblSenha, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE))
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+												.addComponent(passwordField, GroupLayout.DEFAULT_SIZE, 108,
+														Short.MAX_VALUE)
+												.addComponent(textField, GroupLayout.DEFAULT_SIZE, 85,
+														Short.MAX_VALUE))))
+								.addGap(139)));
+		gl_contentPane
+				.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+						gl_contentPane.createSequentialGroup()
+								.addComponent(lblLogin, GroupLayout.PREFERRED_SIZE, 77, Short.MAX_VALUE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(textField)
+										.addComponent(lblUsurio, GroupLayout.PREFERRED_SIZE, 20, Short.MAX_VALUE))
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(passwordField)
+										.addComponent(lblSenha, GroupLayout.DEFAULT_SIZE, 23, Short.MAX_VALUE))
+								.addGap(18)
+								.addComponent(btnEntrar, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+								.addGap(10)));
+		contentPane.setLayout(gl_contentPane);
 	}
 }
